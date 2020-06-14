40c9eb9730106e743983317b00ee1bdd818ecf3c
Abe <abhi.rawat456@gmail.com>
2020-06-14 13:18:27 +0530

    Added javascript

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ var ball = document.querySelector(".ball");
 var count = 0;
 
 switchButton.addEventListener("click", () => {
-  if (count % 2 === 0) {
+  if (count === 0) {
     ball.style.left = "28px";
+    count = 1;
   } else {
     ball.style.left = "4px";
+    count = 0;
   }
-  count++;
 });
